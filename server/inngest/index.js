b354d9f0a7cdf6f85b569a890384a84db389f6f7
Sikboy79 <custombasewelds@yahoo.com>
2025-10-27 19:36:36 -0600

    store user

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,7 +1,25 @@
 import { Inngest } from "inngest";
+import prisma from "../configs/prisma.js";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "my-app" });
+export const inngest = new Inngest({ id: "project-management" });
+
+//Inngest Function to save user data to database
+const syncUserCreation = inngest.createFunction(
+    {id: 'sync-user-from-clerk'},
+    {event: 'clerk/user.created'},
+    async ({ event })=> {
+        const {data} = event
+        await prisma.user.create({
+            data: {
+                id: data.id,
+                email: data.email_addresses[0]?.email_addres,
+                name: data?.first_name + ' ' + data?.last_name,
+                image: data?.image_url,
+            }
+        })
+    }
+)
 
 // Create an empty array where we'll export future Inngest functions
 export const functions = [];
