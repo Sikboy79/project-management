73e6ff09f0b023d9810eeb09a85196e5195eda30
Sikboy79 <custombasewelds@yahoo.com>
2025-10-27 18:46:09 -0600

    neon implemented

--- server/configs/prisma.js
+++ server/configs/prisma.js
@@ -6,16 +6,6 @@ import { neonConfig } from '@neondatabase/serverless';
 import ws from 'ws';
 neonConfig.webSocketConstructor = ws;
 
-// To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
-// neonConfig.poolQueryViaFetch = true
-
-import 'dotenv/config';
-import { PrismaClient } from '@prisma/client';
-import { PrismaNeon } from '@prisma/adapter-neon';
-import { neonConfig } from '@neondatabase/serverless';
-
-import ws from 'ws';
-neonConfig.webSocketConstructor = ws;
 
 // To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
 // neonConfig.poolQueryViaFetch = true
